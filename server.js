4f155410ad3e25a63eb2bad7454d37de9dd1f13d
Gabriel Costa <gabrielncosta208@gmail.com>
2019-12-10 17:03:26 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir')
+// iniciando o app
+const app = express();
+app.use(express.json());
+
+// iniciando o DB
+mongoose.connect('mongodb://localhost:27017/nodeapi',
+ { useNewUrlParser: true }
+);
+requireDir('./src/models');
+
+// const Product = mongoose.model('Product');
+
+// rotas
+app.use('/api', require('./src/routes'))
+
+app.listen(3001);
